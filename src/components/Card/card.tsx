2c3eba0d9a441d42ed0b85d60ad17c0b019b98fc
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2024-02-22 13:43:54 -0800

    tripadviser api updates

--- src/components/Card/card.tsx
+++ src/components/Card/card.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
 
 interface ITravel {
     location_id: string;
@@ -9,15 +8,10 @@ interface ITravel {
     };
 }
 
-interface CardProps {
-    onLocationNameChange: (name: string) => void;
-}
-
 const apiKey = "6DC8221F0F674C3EBA67FEF064069B35";
-
 const tripAdvisorUrl = `https://api.content.tripadvisor.com/api/v1/location/search?searchQuery=vancouver&category=attractions&language=en&key=${apiKey}`;
 
-const Card: React.FC<CardProps> = ({ onLocationNameChange }) => {
+export default function Card() {
     const [locations, setLocations] = useState<ITravel[]>([]);
 
     useEffect(() => {
@@ -27,15 +21,11 @@ const Card: React.FC<CardProps> = ({ onLocationNameChange }) => {
                 if (response.ok) {
                     const data = await response.json();
                     setLocations(data.data);
-                   
-                    if (data.data.length > 0) {
-                        onLocationNameChange(data.data[0].name);
-                    }
                 } else {
-                    console.error('Failed to fetch locations:');
+                    console.error('ERROR');
                 }
             } catch (error) {
-                console.error('Error fetching locations:', error);
+                console.error('Error');
             }
         };
 
@@ -54,5 +44,3 @@ const Card: React.FC<CardProps> = ({ onLocationNameChange }) => {
         </div>
     );
 }
-
-export default Card;
