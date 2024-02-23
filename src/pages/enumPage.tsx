f53a068eaab2ec7ec4fefc2448a90448d6a9fff9
SaraShio <113650568+SaraShio@users.noreply.github.com>
2024-02-23 11:30:59 -0800

    adding enum page

--- src/pages/enumPage.tsx
+++ src/pages/enumPage.tsx
@@ -1,15 +1,27 @@
 import { useState } from "react"
 
-enum WeatherStatus {
-
+enum Activity {
+    Both = 'Both',
+    Indoor = 'Indoor',
+    Outdoor = 'Outdoor',
 }
 
 export default function EnumPage(){
 
+    const [currentWeather, setCurrentWeather] = useState(Activity.Both);
 
     return(
         <>
-           
+            {
+                Activity.Indoor && <p>Indoor Activity</p>
+            }
+
+            {
+                Activity.Outdoor && <p>Outdoor Activity</p>
+            }
+
+            {currentWeather === Activity.Indoor && <p>You should stay inside today!</p>}
+            {currentWeather === Activity.Outdoor && <p>Go outside today!</p>}
         </>
     )
 }
