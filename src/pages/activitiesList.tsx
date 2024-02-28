d56b4455731fc6d644babf6a6178d09b36514619
cleung222@my.bcit.ca <cleung222@my.bcit.ca>
2024-02-28 11:46:35 -0800

    update outdoor

--- src/pages/activitiesList.tsx
+++ src/pages/activitiesList.tsx
@@ -6,7 +6,7 @@ import Card from "@/components/Card/card";
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col p-8`}>
-      <Header/>
+      <Header />
 
       <div>
         {/* Dynamic Text */}
@@ -16,28 +16,28 @@ export default function Home() {
       <div>
         <p className="current-weather-text">Current weater for [dynamic date]</p>
         <div>
-            {/* dynamic illustration  */}
-            {/* dynamic weather information  */}
+          {/* dynamic illustration  */}
+          {/* dynamic weather information  */}
         </div>
         <div>
-            {/* dynamic weather prediction for 5 days  */}
+          {/* dynamic weather prediction for 5 days  */}
         </div>
       </div>
 
-      <hr/>
+      <hr />
 
       <div>
         <div>
           {/* Search Bar */}
-          <TertiaryButton/>
+          <TertiaryButton />
         </div>
+        {/* <Card/>
         <Card/>
         <Card/>
         <Card/>
-        <Card/>
-        <Card/>
+        <Card/> */}
       </div>
-      
+
     </main>
   );
 }
